a679339eb402367ddcfbf1860c876862dab553b6
valerii <valerii.sargov@gmail.com>
2024-11-27 10:42:57 +0100

    completed exercise 101

--- 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
+++ 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
@@ -2,19 +2,18 @@ package it.unibo.oop.lab.lambda;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
-import static java.util.Collections.emptyList;
-import static java.util.Collections.emptyMap;
 
 /**
  * This class will contain four utility functions on lists and maps, of which the first one is provided as example.
@@ -64,7 +63,11 @@ public final class LambdaUtilities {
         /*
          * Suggestion: consider Optional.filter
          */
-        return emptyList();
+        List<Optional<T>> ret = new ArrayList<>();
+        list.forEach(t -> {
+            ret.add(Optional.ofNullable(t).filter(pre));
+        });
+        return ret;
     }
 
     /**
@@ -83,7 +86,16 @@ public final class LambdaUtilities {
         /*
          * Suggestion: consider Map.merge
          */
-        return emptyMap();
+         final Map<R, Set<T>> map = new HashMap<>();
+        // Set union. Could be implemented as a separate function.
+        // Better implementation available in Google Guava
+        final BiFunction<Set<T>, Set<T>, Set<T>> union = (s1, s2) -> {
+            final var result = new LinkedHashSet<>(s1);
+            result.addAll(s2);
+            return result;
+        };
+        list.forEach(t -> map.merge(op.apply(t), Set.of(t), union));
+        return map;
     }
 
     /**
@@ -104,7 +116,11 @@ public final class LambdaUtilities {
          *
          * Keep in mind that a map can be iterated through its forEach method
          */
-        return emptyMap();
+        Map<K, V> ret = new HashMap<>();
+        map.forEach((k, v) -> {
+            ret.put(k, v.orElse(def.get()));
+        });
+        return ret;
     }
 
     /**
